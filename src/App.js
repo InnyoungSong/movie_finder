6c20259c6fbea2880d7188887c89c6ffd277b0cb
InnyoungSong <innyoungsong@gmail.com>
2025-05-26 15:05:45 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import './App.css';
+//import MovieFinder from './components/moviefinder';
+import Movie from './components/Movie';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchTerm: '',
+            results: [],
+            error: ''
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ searchTerm: event.target.value});
+    }
+    handleSubmit(event) {
+        event.preventDefault();
+        let {searchTerm} = this.state;
+        searchTerm = searchTerm.trim();
+        if (!searchTerm) {
+            return;
+        }
+
+        const checkStatus = (response) => {
+            if (response.ok){
+                return response;
+            }
+            throw new Error ('Request was either 404 or 500');
+        }
+
+        const json = (response) => response.json()
+
+        fetch(`https://www.omdbapi.com/?s=${searchTerm}&apikey=b7da8d63`)
+            .then(checkStatus)
+            .then (json)
+            .then(data => {
+                if (data.Response === 'False'){
+                    throw new Error(data.Error);
+                }
+                if (data.Response === 'True' && data.Search) {
+                    this.setState({results: data.Search, error: ''});
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
+    render() {
+        const {searchTerm, results, error} = this.state;
+
+        return (
+            <div className = "container">
+                <div className = "row">
+                    <div className = "col-12">
+                        <form onSubmit = {this.handleSubmit} className="form-inline my-4">
+                            <input type="text" className="form-control mr-sm-2" placeholder="search by movie name" value={searchTerm} onChange={this.handleChange} />
+                            <button type="submit" className="btn btn-primary">Search</button>
+                        </form>
+                        {(() => {
+                            if (error) {
+                                return error;
+                            }
+                            return results.map((movie) => { 
+                            return <Movie key={movie.imdbID} movie={movie} />; 
+                            })
+                        })()}
+                            
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+//export default MovieFinder;
+
+
+
+export default App;
