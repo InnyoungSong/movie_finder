5045fe87009e102470895cda9a5b6232152813d4
InnyoungSong <innyoungsong@gmail.com>
2025-05-30 21:34:31 -0500

    upgrade

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,67 @@
 import React from 'react';
+import {json, checkStatus} from '../utils';
 
-const Movie = (props) => {
-    const {Title, Year, imdbID, Type, Poster} = props.movie;
+class Movie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        movie: null,
+    }
+  }
 
+  componentDidMount () {
+    fetch(`https://www.omdbapi.com/?i=${this.props.match.params.id}&apikey=b7da8d63`)
+      .then(checkStatus)
+      .then(json)
+      .then((data) => {
+        if (data.Response === 'False') {
+          throw new Error(data.Error);
+        }
+        if (data.Response === 'True') {
+          console.log(data);
+          this.setState({ movie: data, error: '' });
+        }
+      })
+      .catch((error) => {
+        this.setState({ error: error.message });
+        console.log(error);
+      })
+  }
+
+  render() {
+    if (!this.state.movie) {
+      return null;
+    }
+    const { Title, Year, Plot, Director, imdbRating, Poster } = this.state.movie;
+    
     return (
-        <div className="row">
-            <div className="col-4 col-md-3 mb-3">
-                <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
-                <img src={Poster} className="img-fluid" />
-                </a>
-            </div>
-            <div className="col-8 col-md-9 mb-3">
-                <a href={`https://www.imdb.com/title/${imdbID}/`} target="_blank">
-                <h4>{Title}</h4>
-                <p>{Type} | {Year}</p>
-                </a>
-            </div>
-         </div>
+      <div className="container">
+        <div className="row pt-5">
+          <div className="col-6">
+            <h1>{Title}</h1>
+                <ul className="list-unstyled">
+                    <li>
+                        <p>Year: {Year}</p>
+                    </li>
+                    <li>
+                        <p>Director: {Director}</p>
+                    </li>
+                    <li>
+                        <p>Plot: {Plot}</p>
+                    </li>
+                    <li>
+                        <p>imdbRating: {imdbRating} / 10</p>
+                    </li>
+                </ul>
+                <div className="col-6">
+                    <img src={Poster} className="img-fluid" />
+                </div>         
+          </div>
+        </div>
+      </div>
     )
+  }
 }
+
 
 export default Movie;
